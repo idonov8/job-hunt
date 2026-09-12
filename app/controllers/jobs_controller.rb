fc50a54db4312c93f2c9c54c6cb767f55ea60bce
Ido Nov <idonov8@gmail.com>
2026-09-12 19:06:19 +0200

    Improve Go Hunt dashboard and openings

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,8 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.ordered
     @hunter = HunterState.current.data
+    @xp = @hunter["completed"].sum { |item| item["xp"] || 100 }
+    @level = @xp / 500 + 1
     @scan = Scan.order(scanned_on: :desc, id: :desc).first
   end
 end
